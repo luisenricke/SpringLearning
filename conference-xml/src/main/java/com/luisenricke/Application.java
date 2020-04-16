c5805e3c3e6aaeaeb039a56b67bd04564df5466a
Luis Villalobos <luisevm24@outlook.com>
2020-04-16 01:14:25 -0500

    Copy project to use XML configurations

---
+++ conference-xml/src/main/java/com/luisenricke/Application.java
@@ -0,0 +1,11 @@
+package com.luisenricke;
+
+import com.luisenricke.service.SpeakerService;
+import com.luisenricke.service.SpeakerServiceImp;
+
+public class Application {
+    public static void main(String[] args) {
+        SpeakerService service = new SpeakerServiceImp();
+        System.out.println(service.findAll().get(0).getFirstName());
+    }
+}
