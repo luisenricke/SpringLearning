eb24a68f79d7088a05e38883805e9b59e243d777
Luis Villalobos <luisevm24@outlook.com>
2020-04-16 02:49:34 -0500

    Add instructions to run profiles

--- conference-java/src/main/java/Application.java
+++ conference-java/src/main/java/Application.java
@@ -3,6 +3,7 @@ import com.luisenricke.service.SpeakerServiceImp;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+// To make environment explicit set this to build in "VM Options": -Dspring.profiles.active=dev
 public class Application {
     public static void main(String[] args) {
         ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
