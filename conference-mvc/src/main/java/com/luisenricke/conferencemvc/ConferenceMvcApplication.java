ae810cc3fcb879c5f62065acdf17cf9f36ed11ee
Luis Villalobos <luisevm24@outlook.com>
2020-04-20 23:37:58 -0500

    Add empty project for MVC Pattern

---
+++ conference-mvc/src/main/java/com/luisenricke/conferencemvc/ConferenceMvcApplication.java
@@ -0,0 +1,13 @@
+package com.luisenricke.conferencemvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceMvcApplication.class, args);
+	}
+
+}
