193b63ee69da7ca1f80a089e01aed42006041c8b
Luis Villalobos <luisevm24@outlook.com>
2020-04-22 01:43:17 -0500

    Fix compile system to .war

--- conference-mvc/src/main/java/com/luisenricke/conferencemvc/ConferenceMvcApplication.java
+++ conference-mvc/src/main/java/com/luisenricke/conferencemvc/ConferenceMvcApplication.java
@@ -2,12 +2,13 @@ package com.luisenricke.conferencemvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ConferenceMvcApplication {
+public class ConferenceMvcApplication extends SpringBootServletInitializer{
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConferenceMvcApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceMvcApplication.class, args);
+    }
 
 }
