946d68edcacdf5e8a4d4a0305212111c5a977699
Luis Villalobos <luisevm24@outlook.com>
2020-04-23 14:21:08 -0500

    Add registration page

---
+++ conference-mvc/src/main/java/com/luisenricke/conferencemvc/controller/RegistrationController.java
@@ -0,0 +1,15 @@
+package com.luisenricke.conferencemvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+
+    @GetMapping("registration")
+    public String getRegistration(Map<String, Object> model) {
+        return "registration";
+    }
+}
