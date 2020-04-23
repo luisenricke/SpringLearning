85152ddf926301c370f4e27d2c9f6713170ad206
Luis Villalobos <luisevm24@outlook.com>
2020-04-23 15:27:58 -0500

    Add route to share static resource

--- conference-mvc/src/main/java/com/luisenricke/conferencemvc/AppConfig.java
+++ conference-mvc/src/main/java/com/luisenricke/conferencemvc/AppConfig.java
@@ -3,10 +3,18 @@ package com.luisenricke.conferencemvc;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("file/**")
+                .addResourceLocations("/WEB-INF/pdf/");
+    }
 
     @Bean
     public ViewResolver viewResolver() {
