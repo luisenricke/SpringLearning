2feabdf9f36e1e10e95317134d37596104c5ce20
Luis Villalobos <luisevm24@outlook.com>
2020-04-23 14:56:15 -0500

    Add simple form with POST

--- conference-mvc/src/main/java/com/luisenricke/conferencemvc/controller/RegistrationController.java
+++ conference-mvc/src/main/java/com/luisenricke/conferencemvc/controller/RegistrationController.java
@@ -1,7 +1,10 @@
 package com.luisenricke.conferencemvc.controller;
 
+import com.luisenricke.conferencemvc.model.Registration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Map;
 
@@ -9,7 +12,13 @@ import java.util.Map;
 public class RegistrationController {
 
     @GetMapping("registration")
-    public String getRegistration(Map<String, Object> model) {
+    public String getRegistration(@ModelAttribute("registration") Registration registration) {
+        return "registration";
+    }
+
+    @PostMapping("registration")
+    public String addRegistration(@ModelAttribute("registration") Registration registration) {
+        System.out.println("Registration: " + registration.getName());
         return "registration";
     }
 }
--- conference-mvc/src/main/webapp/WEB-INF/jsp/registration.jsp
+++ conference-mvc/src/main/webapp/WEB-INF/jsp/registration.jsp
@@ -1,7 +1,25 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <html>
 <head>
-  <title>Registration</title>
+	<title>Registration</title>
+	<form:form modelAttribute="registration">
+	<table>
+		<tr>
+			<td>
+				Name:
+			</td>
+			<td>
+				<form:input path="name"/>
+			</td>
+		</tr>
+		<tr>
+			<td colspan="2">
+				<input type="submit" value="Add Registration">
+			</td>
+		</tr>
+	</table>
+</form:form>
 </head>
 <body>
 
