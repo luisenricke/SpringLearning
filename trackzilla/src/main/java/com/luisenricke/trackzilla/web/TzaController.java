c2508cf3608d3de7b8392d75a522a4d4c7580b43
Luis Villalobos <luisevm24@outlook.com>
2020-04-25 02:08:48 -0500

    Fix controller to make REST standart

--- trackzilla/src/main/java/com/luisenricke/trackzilla/web/TzaController.java
+++ trackzilla/src/main/java/com/luisenricke/trackzilla/web/TzaController.java
@@ -1,14 +1,26 @@
 package com.luisenricke.trackzilla.web;
 
+import com.luisenricke.trackzilla.entity.Application;
+import com.luisenricke.trackzilla.entity.Ticket;
+import com.luisenricke.trackzilla.exception.ApplicationNotFoundException;
 import com.luisenricke.trackzilla.service.ApplicationService;
 import com.luisenricke.trackzilla.service.ReleaseService;
 import com.luisenricke.trackzilla.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/tza")
 public class TzaController {
 
     private ApplicationService applicationService;
@@ -30,21 +42,24 @@ public class TzaController {
         this.releaseService = releaseService;
     }
 
-    @GetMapping("/applications")
-    public String retrieveApplications(Model model) {
-        model.addAttribute("applications", applicationService.listApplications());
-        return "applications";
-    }
-
     @GetMapping("/tickets")
-    public String retrieveTickets(Model model) {
-        model.addAttribute("tickets", ticketService.listTickets());
-        return "tickets";
+    public ResponseEntity<List<Ticket>> getTickets() {
+        List<Ticket> list = ticketService.listTickets();
+        return new ResponseEntity<List<Ticket>>(list, HttpStatus.OK);
     }
 
-    @GetMapping("/releases")
-    public String retrieveReleases(Model model) {
-        model.addAttribute("releases", releaseService.listReleases());
-        return "releases";
+    @GetMapping("/applications")
+    public ResponseEntity<List<Application>> getApplications() {
+        List<Application> list = applicationService.listApplications();
+        return new ResponseEntity<List<Application>>(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/application/{id}")
+    public ResponseEntity<Application> getApplication(@PathVariable("id") long id) {
+        try {
+            return new ResponseEntity<Application>(applicationService.findApplication(id), HttpStatus.OK);
+        } catch (ApplicationNotFoundException ex) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Application not found");
+        }
     }
 }
