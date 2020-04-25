920181c6d09613e5a1de76d49b6d3a093358277e
Luis Villalobos <luisevm24@outlook.com>
2020-04-25 14:49:44 -0500

    Rename PersistenceConfiguration

--- ridetracker/src/main/java/com/luisenricke/ridetracker/config/PersistenceConfiguration.java
+++ ridetracker/src/main/java/com/luisenricke/ridetracker/config/PersistenceConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import javax.sql.DataSource;
 
 @Configuration
-public class PersistanceConfiguration {
+public class PersistenceConfiguration {
 
     @Bean
     public DataSource mySQL() {
